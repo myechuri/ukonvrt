624dd16f0c76de25de6516fd7b236d30d30be2d0
Madhuri Yechuri <cosmokramer@gmail.com>
2016-09-19 17:49:30 -0700

    add support for java apps

--- ukonvrt/create-osv-image.sh
+++ ukonvrt/create-osv-image.sh
@@ -4,20 +4,33 @@ set -ex
 
 # Create Capstanfile
 BASE=/osv
-APP_NAME=${APP##*/}
+# APP_NAME=${APP##*/}
+
+APP_NAME=$(basename "$APP")
+APP_EXTENSION="${APP_NAME##*.}"
+APP_NAME="${APP_NAME%.*}"
+
+if [ $APP_EXTENSION == '' ]; then
+    BASE_IMG=osv-base
+    CMDLINE=$APP
+elif [ $APP_EXTENSION == 'jar' ]; then
+    BASE_IMG=osv-openjdk
+    CMDLINE="/java.so -cp $APP $JAVA_CMD"
+else
+    echo "Unsupported Application format $APP_EXTENSION"
+    exit 1
+fi
+
 cat > ${BASE}/Capstanfile << EOF
-base: cloudius/osv-base
+# OSv base image.
+base: cloudius/$BASE_IMG
 
-#
 # The command line passed to OSv to start up the application.
-#
-cmdline: /tools/hello.so
+cmdline: $CMDLINE
 
-#
 # List of files that are included in the generated image.
-#
 files:
-  /tools/$APP_NAME.so: $APP 
+  $APP: $APP 
 EOF
 
 # run "capstan build" to generate OSv image
